75a87ddfe19ce6638380d9e139bb40e3fb1a080f
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-22 18:37:58 +0300

    Add scoped firebase client to IoC

--- Sources/Program.cs
+++ Sources/Program.cs
@@ -3,12 +3,15 @@ using System.Net.Http;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorFirebaseTest;
+using Firebase.Database;
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+    .AddScoped<FirebaseClient>(sp 
+        => new FirebaseClient("https://asptest-1c6c7-default-rtdb.europe-west1.firebasedatabase.app/"));
 
 await builder.Build().RunAsync();
