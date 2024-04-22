f0867b0dd6192008ab24e79380354e12580b9f82
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-22 18:55:34 +0300

    Add Create and Delete

--- Sources/Models/Person.cs
+++ Sources/Models/Person.cs
@@ -1,8 +1,8 @@
 ﻿namespace BlazorFirebaseTest.Models;
 
-public class Person(int Id, string FirstName, string LastName)
+public class Person(string Id, string FirstName, string LastName)
 {
-    public int Id { get; set; } = Id;
+    public string Id { get; set; } = Id;
     public string FirstName { get; set; } = FirstName;
     public string LastName { get; set; } = LastName;
 }
