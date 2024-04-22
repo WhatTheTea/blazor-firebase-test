30f722df8ef645964e68479b15b54e5c444c03f0
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-22 18:38:06 +0300

    Change Person to class

--- Sources/Models/Person.cs
+++ Sources/Models/Person.cs
@@ -1,3 +1,8 @@
 ﻿namespace BlazorFirebaseTest.Models;
 
-public record Person(int Id, string FirstName, string LastName);
+public class Person(int Id, string FirstName, string LastName)
+{
+    public int Id { get; set; } = Id;
+    public string FirstName { get; set; } = FirstName;
+    public string LastName { get; set; } = LastName;
+}
